1b67ac049f88bb76db38f43ce987f88d940cde77
Maria Paula Velasquez-Garcia <maria960806@gmail.com>
2024-03-20 16:18:06 +0000

    Excersice about classes to make the code even more versatil :)

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -19,7 +19,16 @@ def main(args):
         InFiles = [args.infiles]
     
     if args.full_data_analysis:
-        daily_standard_deviation = compute_data.analyse_data(os.path.dirname(InFiles[0]))
+        _, extension = os.path.splitext(InFiles[0])
+        if extension == '.json':
+            data_source_json = compute_data.JSONDataSourceDataSource(os.path.dirname(InFiles[0]))
+        elif extension == '.csv':
+            data_source_csv = compute_data.CSVDataSource(os.path.dirname(InFiles[0]))
+        else:
+            raise ValueError(f'Unsupported file format: {extension}')
+        
+        daily_standard_deviation = compute_data.analyse_data(data_source)
+        #daily_standard_deviation = compute_data.analyse_data(os.path.dirname(InFiles[0]))
         graph_data = {
             'daily standard deviation': daily_standard_deviation
         }
--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -8,31 +8,45 @@ import pandas as pd
 from catchment import models, views
 
 
-def analyse_data(data_dir):
-    """Calculate the standard deviation by day between datasets.
-
-    Gets all the measurement data from the CSV files in the data directory,
-    works out the mean for each day, and then graphs the standard deviation
+def analyse_data(dataset):
+    """works out the mean for each day, and then graphs the standard deviation
     of these means.
     """
-    #data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
-    #if len(data_file_paths) == 0:
-    data_file_paths = load_catchment_data(data_dir)    
-    data = map(models.read_variable_from_csv, data_file_paths)
+    #= CSVDataSource(data_dir)
+    data = dataset.load_catchment_data() 
+
+    #data = CSV_data.load_catchment_data() 
     daily_standard_deviation = compute_standard_deviation_by_day(data)
     return daily_standard_deviation
 
-def load_catchment_data(data_dir):
-    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError('No CSV files found in the data directory')
-    return data_file_paths
+class CSVDataSource:
+    def __init__(self,data_dir):
+        self.data_dir = data_dir
+        
+    def load_catchment_data(self):
+        """"Gets all the measurement data from the CSV files in the data directory"""
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'rain_data_2015*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError('No CSV files found in the data directory')
+        data = map(models.read_variable_from_csv, data_file_paths)
+        return data
+
+class JSONDataSource:
+    def __init__(self,data_dir):
+        self.data_dir = data_dir
+        
+    def load_catchment_data(self):
+        """"Gets all the measurement data from the CSV files in the data directory"""
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'rain_data_2015*.json'))
+        if len(data_file_paths) == 0:
+            raise ValueError('No JSON files found in the data directory')
+        data = map(models.read_variable_from_json, data_file_paths)
+        return data
+
+#read_variable_from_json
 
 def compute_standard_deviation_by_day(data):
-    #daily_std_list = []
+    """Calculate the standard deviation by day between datasets"""
     daily_std_list = map(models.daily_std, data)
-    #for dataset in data:
-    #    daily_std = dataset.groupby(dataset.index.date).std()
-    #    daily_std_list.append(daily_std)
     return  pd.concat(daily_std_list)
     
