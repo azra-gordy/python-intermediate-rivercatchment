74c09d15831b6f0baca5c6ef63ba28ed7fae82f7
Azra Gordy <azra.gordy@jncc.gov.uk>
2024-03-20 16:13:16 +0000

    Abstract classes and use object-oriented programming to allow the analysis to also take JSON input

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -8,13 +8,30 @@ import pandas as pd
 from catchment import models, views
 
 
-def load_catchment_data(data_dir):
-    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
-    if len(data_file_paths) == 0:
-       raise ValueError('No CSV files found in the data directory')
-    data = map(models.read_variable_from_csv, data_file_paths)
+class CSVDataSource:
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
 
-    return data
+    def load_catchment_data(self):
+        data_file_paths = glob.glob(os.path.join(self.dir_path, 'rain_data_2015*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError('No CSV files found in data directory')
+        data = map(models.read_variable_from_csv, data_file_paths)
+
+        return data
+
+
+class JSONDataSource:
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+
+    def load_catchment_data(self):
+        data_file_paths = glob.glob(os.path.join(self.dir_path, 'rain_data_2015*.json'))
+        if len(data_file_paths) == 0:
+            raise ValueError('No JSON files found in data directory')
+        data = map(models.read_variable_from_json, data_file_paths)
+
+        return data
 
 
 def daily_std(data):
@@ -30,14 +47,14 @@ def compute_standard_deviation_by_day(data_list):
     return daily_standard_deviation
 
 
-def analyse_data(data_dir):
+def analyse_data(data_source):
     """Calculate the standard deviation by day between datasets.
 
     Gets all the measurement data from the CSV files in the data directory,
     works out the mean for each day, and then graphs the standard deviation
     of these means.
     """
-    data = load_catchment_data(data_dir)
+    data = data_source.load_catchment_data()
     return compute_standard_deviation_by_day(data)
 
     # graph_data = {
