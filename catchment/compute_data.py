c8f322fcd071bc9609d20c8f1eb9558b7a66a674
Azra Gordy <azra.gordy@jncc.gov.uk>
2024-03-20 14:32:51 +0000

    Refactor and abstract

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -8,6 +8,28 @@ import pandas as pd
 from catchment import models, views
 
 
+def load_catchment_data(data_dir):
+    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
+    if len(data_file_paths) == 0:
+       raise ValueError('No CSV files found in the data directory')
+    data = map(models.read_variable_from_csv, data_file_paths)
+
+    return data
+
+
+def daily_std(data):
+    """Calculate the standard deviation by day of a 2D data array"""
+    return data.groupby(data.index.date).std()
+
+
+def compute_standard_deviation_by_day(data_list):
+    daily_std_list = map(daily_std, data_list)
+
+    daily_standard_deviation = pd.concat(daily_std_list)
+
+    return daily_standard_deviation
+
+
 def analyse_data(data_dir):
     """Calculate the standard deviation by day between datasets.
 
@@ -15,16 +37,32 @@ def analyse_data(data_dir):
     works out the mean for each day, and then graphs the standard deviation
     of these means.
     """
-    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError('No CSV files found in the data directory')
-    data = map(models.read_variable_from_csv, data_file_paths)
+    data = load_catchment_data(data_dir)
+    return compute_standard_deviation_by_day(data)
 
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
+    # graph_data = {
+    #     'standard deviation by day': daily_standard_deviation,
+    #     }
+    # views.visualize(graph_data)
+
+
+# def analyse_data(data_dir):
+#     """Calculate the standard deviation by day between datasets.
+
+#     Gets all the measurement data from the CSV files in the data directory,
+#     works out the mean for each day, and then graphs the standard deviation
+#     of these means.
+#     """
+#     data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
+#     if len(data_file_paths) == 0:
+#         raise ValueError('No CSV files found in the data directory')
+#     data = map(models.read_variable_from_csv, data_file_paths)
+
+#     daily_std_list = []
+#     for dataset in data:
+#         daily_std = dataset.groupby(dataset.index.date).std()
+#         daily_std_list.append(daily_std)
     
-    daily_standard_deviation = pd.concat(daily_std_list)
+#     daily_standard_deviation = pd.concat(daily_std_list)
 
-    return daily_standard_deviation
+#     return daily_standard_deviation
