9aa46d6538615c2ddf56546d0e801ed34b9d9a2a
Jo316 <l.joseph.p@hotmail.com>
2024-03-21 12:19:22 +0000

    feat: xml function read and data

--- catchment/models.py
+++ catchment/models.py
@@ -81,3 +81,31 @@ def data_normalise(data):
     """Normalise any given 2D data array"""
     normal_max = np.array(np.max(data, axis=0))
     return data / normal_max[np.newaxis, :]
+
+
+def read_variable_from_xml(filename):
+    """Reads a named variable from a XML file, and returns a
+    pandas dataframe containing that variable. The XML file must contain
+    a column of dates, a column of site ID's, and (one or more) columns
+    of data - only one of which will be read.
+
+    :param filename: Filename of CSV to load
+    :return: 2D array of given variable. Index will be dates,
+             Columns will be the individual sites
+    """
+    dataset = pd.read_xml(filename)
+    dataset = dataset.rename({'Date':'OldDate', 'Site_Name':'Site Name', 
+                              'Rainfall_mm':'Rainfall (mm)'}, axis='columns')
+    
+    dataset['Date'] = [pd.to_datetime(x,dayfirst=True) for x in dataset['OldDate']]
+    dataset = dataset.drop('OldDate', axis='columns')
+
+    #dataset = dataset.rename({'Date':'OldDate'}, axis='columns')
+    newdataset = pd.DataFrame(index=dataset['Date'].unique())
+
+    for site in dataset['Site'].unique():
+        newdataset[site] = dataset[dataset['Site'] == site].set_index('Date')["Rainfall (mm)"]
+
+    newdataset = newdataset.sort_index()
+
+    return newdataset
