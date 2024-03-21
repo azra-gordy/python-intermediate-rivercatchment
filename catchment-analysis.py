7851cb1fc33b481cb09d819e93c672c3bc09741b
Jo316 <l.joseph.p@hotmail.com>
2024-03-21 10:37:06 +0000

    fix: function to includes other variables from other files

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -38,7 +38,7 @@ def main(args):
         views.visualize(graph_data)
 
     for filename in InFiles:
-        measurement_data = models.read_variable_from_csv(filename)
+        measurement_data = models.read_variable_from_csv(filename, args.measurements)
  
         view_data = {'daily sum': models.daily_total(measurement_data),
                      'daily average': models.daily_mean(measurement_data),
@@ -53,12 +53,22 @@ def create_argparse():
     parser = argparse.ArgumentParser(
         description='A basic environmental data management system')
     
+    req_group = parser.add_argument_group('required arguments')
+    
     parser.add_argument(
         'infiles',
         nargs='+',
         help='Input CSV(s) containing measurement data')
 
-    parser.add_argument('--full-data-analysis', action='store_true', dest='full_data_analysis')
+    parser.add_argument(
+        '--full-data-analysis', 
+        action='store_true', 
+        dest='full_data_analysis')
+    
+    req_group.add_argument(
+        '-m', '--measurements',
+        help='Name of the measurement data series to load',
+    )
     return parser
 
 
